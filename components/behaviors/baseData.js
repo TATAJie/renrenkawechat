c59f01d684cd3029f324be76657d9e059012baef
unknown <unknown@example.com>
2020-03-19 10:27:49 +0800

    我的适配

--- components/behaviors/baseData.js
+++ components/behaviors/baseData.js
@@ -1,7 +1,8 @@
 const App = getApp();
 const baseDataBev = Behavior({
   data:{
-    contentHeight: App.globalData.screenHeight - App.globalData.statusBarHeight - App.globalData.navigationHeight - App.globalData.tabBarHeight
+    contentHeight: App.globalData.screenHeight - App.globalData.statusBarHeight - App.globalData.navigationHeight - App.globalData.tabBarHeight,
+    contentHeightWithoutNav: App.globalData.screenHeight - App.globalData.tabBarHeight
   }
 })
 export{
